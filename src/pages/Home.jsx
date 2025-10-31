6f9057b90d3b56dc4ab59838db1fff432981effc
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-10-31 10:42:03 +0700

    `Removed unused dependencies and components, updated SEO metadata and favicon`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,10 @@ import Experiences from '../sections/Home/Experiences'
 import Projects from '../sections/Home/Projects'
 import Contacts from '../sections/Home/Contacts'
 import Footer from '../sections/Home/Footer'
-import SEO from '../components/SEO'
-import icon from '../assets/favicon.svg'
 
 function Home() {
     return (
       <main className='flex flex-col space-y-30'>
-        <SEO  image={icon} type="website" url="https://sevalino.netlify.app/" name="Sevalino Elfata" title="Sevalino Elfata | Software Engineer" description="I'm Sevalino Elfata, a Software Engineering student at IDN Boarding School, passionate about crafting clean, efficient, and user-focused applications." />
         <Hero />
         <AboutUs />
         <Experiences />
